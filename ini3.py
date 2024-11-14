3c81ee7fa4946b220de690cc9f16f785f3f20ae1
Marko-SE <a01142841@unet.univie.ac.at>
2024-11-14 17:35:12 +0100

    Added homework ini3

---
+++ ini3.py
@@ -0,0 +1,8 @@
+## Homework INI3 Rosalind 2024_11_15
+
+~~~ Python
+    ## 79 86 111 121
+    s = "nMvgWTsLJPSmKtuLDbUmEgoJJUA2eHFi2smzEEg9IvxfLQTIK5tmH2oexMfA32ueiL6pKv0bI4IgOExArenaria5mgI0DkTZeONXaaKh6HBUFydtroglodytesCgQYvLMyjlcEIgXiVcHH45HEJySSpOu73zWEdjGId2vm."
+    result=s[79:87] + " " + s[111:122]
+    print(result)
+~~~
