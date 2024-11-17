503fd7c5f536752f500a257e3a9b957b5381da17
Marko-SE <a01142841@unet.univie.ac.at>
2024-11-17 18:36:43 +0100

    ini6 homework

---
+++ ini6.py
@@ -0,0 +1,18 @@
+# Homewordk ini6
+~~~ Python
+sentence = "When I find myself in times of trouble Mother Mary comes to me Speaking words of wisdom let it be And in my hour of darkness she is standing right in front of me Speaking words of wisdom let it be Let it be let it be let it be let it be Whisper words of wisdom let it be And when the broken hearted people living in the world agree There will be an answer let it be For though they may be parted there is still a chance that they will see There will be an answer let it be Let it be let it be let it be let it be There will be an answer let it be Let it be let it be let it be let it be Whisper words of wisdom let it be Let it be let it be let it be let it be Whisper words of wisdom let it be And when the night is cloudy there is still a light that shines on me Shine until tomorrow let it be I wake up to the sound of music Mother Mary comes to me Speaking words of wisdom let it be Let it be let it be let it be yeah let it be There will be an answer let it be Let it be let it be let it be yeah let it be Whisper words of wisdom let it be"
+words = sentence.split()
+print(words)
+
+word_counts = {}
+for word in words:
+    if word in word_counts:
+        word_counts[word] += 1 
+    else:
+        word_counts[word] = 1  
+
+print(word_counts)
+
+for key, value in word_counts.items():
+    print(key, value)
+~~~
