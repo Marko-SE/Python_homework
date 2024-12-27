b60c03addabbc1da1e2d3261f31b2508d7edb2c5
Marko-SE <a01142841@unet.univie.ac.at>
2024-12-27 12:15:42 +0100

    fibonacci 2 simplified

--- fibd2.py
+++ fibd2.py
@@ -1,8 +1,6 @@
 def num_rabbits(n, m):
-    num_list = []
-    num_list.append(0)
-    num_list.append(1)
-    for i in range(1, n+1, 1):
+    num_list = [0, 1]
+    for i in range(1, n, 1):
         if i < m:
             num_list.append(num_list[i] + num_list[i-1])
         if i == m:
